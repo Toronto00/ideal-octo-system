7ab112b54350129451759c82e6fa078df900b592
Toronto00 <66029280+Toronto00@users.noreply.github.com>
2020-11-13 14:28:30 +0100

    Initialize chrmarti/85328

---
+++ src/vs/workbench/contrib/customEditor/browser/customEditorInput.ts
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { memoize } from 'vs/base/common/decorators';
+import { Lazy } from 'vs/base/common/lazy';
+import { UnownedDisposable } from 'vs/base/common/lifecycle';
+import { basename } from 'vs/base/common/path';
+import { isEqual } from 'vs/base/common/resources';
+import { URI } from 'vs/base/common/uri';
+import { IEditorModel } from 'vs/platform/editor/common/editor';
+import { ILabelService } from 'vs/platform/label/common/label';
+import { ILifecycleService } from 'vs/platform/lifecycle/common/lifecycle';
+import { GroupIdentifier, IEditorInput, IRevertOptions, ISaveOptions, Verbosity } from 'vs/workbench/common/editor';
+import { ICustomEditorModel, ICustomEditorService } from 'vs/workbench/contrib/customEditor/common/customEditor';
+import { WebviewEditorOverlay } from 'vs/workbench/contrib/webview/browser/webview';
+import { IWebviewWorkbenchService, LazilyResolvedWebviewEditorInput } from 'vs/workbench/contrib/webview/browser/webviewWorkbenchService';
+
+export class CustomFileEditorInput extends LazilyResolvedWebviewEditorInput {
+
+	public static typeId = 'workbench.editors.webviewEditor';
+
+	private readonly _editorResource: URI;
+	private _model?: ICustomEditorModel;
+
+	constructor(
+		resource: URI,
+		viewType: string,
+		id: string,
+		webview: Lazy<UnownedDisposable<WebviewEditorOverlay>>,
+		@ILifecycleService lifecycleService: ILifecycleService,
+		@IWebviewWorkbenchService webviewWorkbenchService: IWebviewWorkbenchService,
+		@ILabelService private readonly labelService: ILabelService,
+		@ICustomEditorService private readonly customEditorService: ICustomEditorService,
+	) {
+		super(id, viewType, '', webview, webviewWorkbenchService, lifecycleService);
+		this._editorResource = resource;
+	}
+
+	public getTypeId(): string {
+		return CustomFileEditorInput.typeId;
+	}
+
+	public getResource(): URI {
+		return this._editorResource;
+	}
+
+	@memoize
+	getName(): string {
+		return basename(this.labelService.getUriLabel(this.getResource()));
+	}
+
+	@memoize
+	getDescription(): string | undefined {
+		return super.getDescription();
+	}
+
+	matches(other: IEditorInput): boolean {
+		return this === other || (other instanceof CustomFileEditorInput
+			&& this.viewType === other.viewType
+			&& isEqual(this.getResource(), other.getResource()));
+	}
+
+	@memoize
+	private get shortTitle(): string {
+		return this.getName();
+	}
+
+	@memoize
+	private get mediumTitle(): string {
+		return this.labelService.getUriLabel(this.getResource(), { relative: true });
+	}
+
+	@memoize
+	private get longTitle(): string {
+		return this.labelService.getUriLabel(this.getResource());
+	}
+
+	public getTitle(verbosity?: Verbosity): string {
+		switch (verbosity) {
+			case Verbosity.SHORT:
+				return this.shortTitle;
+			default:
+			case Verbosity.MEDIUM:
+				return this.mediumTitle;
+			case Verbosity.LONG:
+				return this.longTitle;
+		}
+	}
+
+	public isReadonly(): boolean {
+		return false;
+	}
+
+	public isDirty(): boolean {
+		return this._model ? this._model.isDirty() : false;
+	}
+
+	public save(groupId: GroupIdentifier, options?: ISaveOptions): Promise<boolean> {
+		return this._model ? this._model.save(options) : Promise.resolve(false);
+	}
+
+	public saveAs(groupId: GroupIdentifier, options?: ISaveOptions): Promise<boolean> {
+		// TODO@matt implement properly (see TextEditorInput#saveAs())
+		return this._model ? this._model.save(options) : Promise.resolve(false);
+	}
+
+	public revert(options?: IRevertOptions): Promise<boolean> {
+		return this._model ? this._model.revert(options) : Promise.resolve(false);
+	}
+
+	public async resolve(): Promise<IEditorModel> {
+		this._model = await this.customEditorService.models.loadOrCreate(this.getResource(), this.viewType);
+		this._register(this._model.onDidChangeDirty(() => this._onDidChangeDirty.fire()));
+		return await super.resolve();
+	}
+}

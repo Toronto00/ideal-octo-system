e6e90116a6cfd3ae211de6d1ff48f413eccdf79b
Toronto00 <66029280+Toronto00@users.noreply.github.com>
2020-11-13 14:58:42 +0100

    Initialize connor4312/debug-menu-context-keys

---
+++ src/vs/platform/userDataSync/common/userDataAutoSyncService.ts
@@ -0,0 +1,158 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { timeout, Delayer } from 'vs/base/common/async';
+import { Event, Emitter } from 'vs/base/common/event';
+import { Disposable } from 'vs/base/common/lifecycle';
+import { IUserDataSyncLogService, IUserDataSyncService, SyncStatus, IUserDataAutoSyncService, UserDataSyncError, UserDataSyncErrorCode, IUserDataSyncEnablementService, ALL_SYNC_RESOURCES } from 'vs/platform/userDataSync/common/userDataSync';
+import { IAuthenticationTokenService } from 'vs/platform/authentication/common/authentication';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+
+type AutoSyncClassification = {
+	sources: { classification: 'SystemMetaData', purpose: 'FeatureInsight', isMeasurement: true };
+};
+
+export const RESOURCE_ENABLEMENT_SOURCE = 'resourceEnablement';
+
+export class UserDataAutoSyncService extends Disposable implements IUserDataAutoSyncService {
+
+	_serviceBrand: any;
+
+	private enabled: boolean = this.getDefaultEnablementValue();
+	private successiveFailures: number = 0;
+	private lastSyncTriggerTime: number | undefined = undefined;
+	private readonly syncDelayer: Delayer<void>;
+
+	private readonly _onError: Emitter<UserDataSyncError> = this._register(new Emitter<UserDataSyncError>());
+	readonly onError: Event<UserDataSyncError> = this._onError.event;
+
+	constructor(
+		@IUserDataSyncEnablementService private readonly userDataSyncEnablementService: IUserDataSyncEnablementService,
+		@IUserDataSyncService private readonly userDataSyncService: IUserDataSyncService,
+		@IUserDataSyncLogService private readonly logService: IUserDataSyncLogService,
+		@IAuthenticationTokenService private readonly authTokenService: IAuthenticationTokenService,
+		@ITelemetryService private readonly telemetryService: ITelemetryService,
+	) {
+		super();
+		this.updateEnablement(false, true);
+		this.syncDelayer = this._register(new Delayer<void>(0));
+		this._register(Event.any<any>(authTokenService.onDidChangeToken)(() => this.updateEnablement(true, true)));
+		this._register(Event.any<any>(userDataSyncService.onDidChangeStatus)(() => this.updateEnablement(true, true)));
+		this._register(this.userDataSyncEnablementService.onDidChangeEnablement(() => this.updateEnablement(true, false)));
+		this._register(Event.filter(this.userDataSyncEnablementService.onDidChangeResourceEnablement, ([, enabled]) => enabled)(() => this.triggerAutoSync([RESOURCE_ENABLEMENT_SOURCE])));
+	}
+
+	// For tests purpose only
+	protected getDefaultEnablementValue(): boolean { return false; }
+
+	private updateEnablement(stopIfDisabled: boolean, auto: boolean): void {
+		const { enabled, reason } = this.isAutoSyncEnabled();
+		if (this.enabled === enabled) {
+			return;
+		}
+
+		this.enabled = enabled;
+		if (this.enabled) {
+			this.logService.info('Auto Sync: Started');
+			this.sync(true, auto);
+			return;
+		} else {
+			this.resetFailures();
+			if (stopIfDisabled) {
+				this.userDataSyncService.stop();
+				this.logService.info('Auto Sync: stopped because', reason);
+			}
+		}
+
+	}
+
+	private async sync(loop: boolean, auto: boolean): Promise<void> {
+		if (this.enabled) {
+			try {
+				this.lastSyncTriggerTime = new Date().getTime();
+				await this.userDataSyncService.sync();
+				this.resetFailures();
+			} catch (e) {
+				const error = UserDataSyncError.toUserDataSyncError(e);
+				if (error.code === UserDataSyncErrorCode.TurnedOff || error.code === UserDataSyncErrorCode.SessionExpired) {
+					this.logService.info('Auto Sync: Sync is turned off in the cloud.');
+					this.logService.info('Auto Sync: Resetting the local sync state.');
+					await this.userDataSyncService.resetLocal();
+					this.logService.info('Auto Sync: Completed resetting the local sync state.');
+					if (auto) {
+						this.userDataSyncEnablementService.setEnablement(false);
+						this._onError.fire(error);
+						return;
+					} else {
+						return this.sync(loop, auto);
+					}
+				}
+				if (error.code === UserDataSyncErrorCode.TooManyRequests) {
+					this.logService.info('Auto Sync: Turned off sync because of making too many requests to server');
+					this.userDataSyncEnablementService.setEnablement(false);
+					this._onError.fire(error);
+					return;
+				}
+				this.logService.error(error);
+				this.successiveFailures++;
+				this._onError.fire(error);
+			}
+			if (loop) {
+				await timeout(1000 * 60 * 5);
+				this.sync(loop, true);
+			}
+		} else {
+			this.logService.trace('Auto Sync: Not syncing as it is disabled.');
+		}
+	}
+
+	private isAutoSyncEnabled(): { enabled: boolean, reason?: string } {
+		if (!this.userDataSyncEnablementService.isEnabled()) {
+			return { enabled: false, reason: 'sync is disabled' };
+		}
+		if (this.userDataSyncService.status === SyncStatus.Uninitialized) {
+			return { enabled: false, reason: 'sync is not initialized' };
+		}
+		if (!this.authTokenService.token) {
+			return { enabled: false, reason: 'token is not avaialable' };
+		}
+		return { enabled: true };
+	}
+
+	private resetFailures(): void {
+		this.successiveFailures = 0;
+	}
+
+	private sources: string[] = [];
+	async triggerAutoSync(sources: string[]): Promise<void> {
+		if (!this.enabled) {
+			return this.syncDelayer.cancel();
+		}
+
+		/*
+		If sync is not triggered by sync resource (triggered by other sources like window focus etc.,) or by resource enablement
+		then limit sync to once per 10s
+		*/
+		const hasToLimitSync = sources.indexOf(RESOURCE_ENABLEMENT_SOURCE) === -1 && ALL_SYNC_RESOURCES.every(syncResource => sources.indexOf(syncResource) === -1);
+		if (hasToLimitSync && this.lastSyncTriggerTime
+			&& Math.round((new Date().getTime() - this.lastSyncTriggerTime) / 1000) < 10) {
+			this.logService.debug('Auto Sync Skipped: Limited to once per 10 seconds.');
+			return;
+		}
+
+		this.sources.push(...sources);
+		return this.syncDelayer.trigger(() => {
+			this.telemetryService.publicLog2<{ sources: string[] }, AutoSyncClassification>('sync/triggered', { sources: this.sources });
+			this.sources = [];
+
+			this.logService.info('Auto Sync: Triggered.');
+			return this.sync(false, true);
+		}, this.successiveFailures
+			? 1000 * 1 * Math.min(Math.pow(2, this.successiveFailures), 60) /* Delay exponentially until max 1 minute */
+			: 0); /* Do not delay if there are no failures */
+
+	}
+
+}
---
+++ src/vs/workbench/contrib/markers/browser/markersModel.ts
@@ -0,0 +1,223 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { basename, compare, getComparisonKey } from 'vs/base/common/resources';
+import { URI } from 'vs/base/common/uri';
+import { Range, IRange } from 'vs/editor/common/core/range';
+import { IMarker, MarkerSeverity, IRelatedInformation, IMarkerData } from 'vs/platform/markers/common/markers';
+import { mergeSort, isNonEmptyArray, flatten } from 'vs/base/common/arrays';
+import { ResourceMap } from 'vs/base/common/map';
+import { Emitter, Event } from 'vs/base/common/event';
+import { Hasher } from 'vs/base/common/hash';
+import { withUndefinedAsNull } from 'vs/base/common/types';
+
+
+export function compareMarkersByUri(a: IMarker, b: IMarker) {
+	return compare(a.resource, b.resource, false, false);
+}
+
+function compareResourceMarkers(a: ResourceMarkers, b: ResourceMarkers): number {
+	let [firstMarkerOfA] = a.markers;
+	let [firstMarkerOfB] = b.markers;
+	let res = 0;
+	if (firstMarkerOfA && firstMarkerOfB) {
+		res = MarkerSeverity.compare(firstMarkerOfA.marker.severity, firstMarkerOfB.marker.severity);
+	}
+	if (res === 0) {
+		res = a.path.localeCompare(b.path) || a.name.localeCompare(b.name);
+	}
+	return res;
+}
+
+function compareMarkers(a: Marker, b: Marker): number {
+	return MarkerSeverity.compare(a.marker.severity, b.marker.severity)
+		|| Range.compareRangesUsingStarts(a.marker, b.marker);
+}
+
+export class ResourceMarkers {
+
+	readonly path: string;
+
+	readonly name: string;
+
+	private markersMap = new ResourceMap<Marker[]>();
+	private _total: number = 0;
+
+	constructor(readonly id: string, readonly resource: URI) {
+		this.path = this.resource.fsPath;
+		this.name = basename(this.resource);
+	}
+
+	get markers(): readonly Marker[] {
+		return flatten([...this.markersMap.values()]);
+	}
+
+	has(uri: URI) {
+		return this.markersMap.has(uri);
+	}
+
+	set(uri: URI, marker: Marker[]) {
+		this.delete(uri);
+		if (isNonEmptyArray(marker)) {
+			this.markersMap.set(uri, marker);
+			this._total += marker.length;
+		}
+	}
+
+	delete(uri: URI) {
+		let array = this.markersMap.get(uri);
+		if (array) {
+			this._total -= array.length;
+			this.markersMap.delete(uri);
+		}
+	}
+
+	get total() {
+		return this._total;
+	}
+}
+
+export class Marker {
+
+	get resource(): URI { return this.marker.resource; }
+	get range(): IRange { return this.marker; }
+
+	private _lines: string[] | undefined;
+	get lines(): string[] {
+		if (!this._lines) {
+			this._lines = this.marker.message.split(/\r\n|\r|\n/g);
+		}
+		return this._lines;
+	}
+
+	constructor(
+		readonly id: string,
+		readonly marker: IMarker,
+		readonly relatedInformation: RelatedInformation[] = []
+	) { }
+
+	toString(): string {
+		return JSON.stringify({
+			...this.marker,
+			resource: this.marker.resource.path,
+			relatedInformation: this.relatedInformation.length ? this.relatedInformation.map(r => ({ ...r.raw, resource: r.raw.resource.path })) : undefined
+		}, null, '\t');
+	}
+}
+
+export class RelatedInformation {
+
+	constructor(
+		readonly id: string,
+		readonly marker: IMarker,
+		readonly raw: IRelatedInformation
+	) { }
+}
+
+export interface MarkerChangesEvent {
+	readonly added: Set<ResourceMarkers>;
+	readonly removed: Set<ResourceMarkers>;
+	readonly updated: Set<ResourceMarkers>;
+}
+
+export class MarkersModel {
+
+	private cachedSortedResources: ResourceMarkers[] | undefined = undefined;
+
+	private readonly _onDidChange = new Emitter<MarkerChangesEvent>();
+	readonly onDidChange: Event<MarkerChangesEvent> = this._onDidChange.event;
+
+	get resourceMarkers(): ResourceMarkers[] {
+		if (!this.cachedSortedResources) {
+			this.cachedSortedResources = [...this.resourcesByUri.values()].sort(compareResourceMarkers);
+		}
+		return this.cachedSortedResources;
+	}
+
+	private resourcesByUri: Map<string, ResourceMarkers>;
+
+	constructor() {
+		this.resourcesByUri = new Map<string, ResourceMarkers>();
+	}
+
+	private _total: number = 0;
+	get total(): number {
+		return this._total;
+	}
+
+	getResourceMarkers(resource: URI): ResourceMarkers | null {
+		return withUndefinedAsNull(this.resourcesByUri.get(getComparisonKey(resource, false, true)));
+	}
+
+	setResourceMarkers(resourcesMarkers: [URI, IMarker[]][]): void {
+		const change: MarkerChangesEvent = { added: new Set(), removed: new Set(), updated: new Set() };
+		for (const [resource, rawMarkers] of resourcesMarkers) {
+
+			const key = getComparisonKey(resource, false, true);
+			let resourceMarkers = this.resourcesByUri.get(key);
+
+			if (isNonEmptyArray(rawMarkers)) {
+				// update, add
+				if (!resourceMarkers) {
+					const resourceMarkersId = this.id(resource.toString());
+					resourceMarkers = new ResourceMarkers(resourceMarkersId, resource.with({ fragment: null }));
+					this.resourcesByUri.set(key, resourceMarkers);
+					change.added.add(resourceMarkers);
+				} else {
+					change.updated.add(resourceMarkers);
+				}
+				const markersCountByKey = new Map<string, number>();
+				const markers = mergeSort(rawMarkers.map((rawMarker) => {
+					const key = IMarkerData.makeKey(rawMarker);
+					const index = markersCountByKey.get(key) || 0;
+					markersCountByKey.set(key, index + 1);
+
+					const markerId = this.id(resourceMarkers!.id, key, index);
+
+					let relatedInformation: RelatedInformation[] | undefined = undefined;
+					if (rawMarker.relatedInformation) {
+						relatedInformation = rawMarker.relatedInformation.map((r, index) => new RelatedInformation(this.id(markerId, r.resource.toString(), r.startLineNumber, r.startColumn, r.endLineNumber, r.endColumn, index), rawMarker, r));
+					}
+
+					return new Marker(markerId, rawMarker, relatedInformation);
+				}), compareMarkers);
+
+				this._total -= resourceMarkers.total;
+				resourceMarkers.set(resource, markers);
+				this._total += resourceMarkers.total;
+
+			} else if (resourceMarkers) {
+				// clear
+				this._total -= resourceMarkers.total;
+				resourceMarkers.delete(resource);
+				this._total += resourceMarkers.total;
+				if (resourceMarkers.total === 0) {
+					this.resourcesByUri.delete(key);
+					change.removed.add(resourceMarkers);
+				} else {
+					change.updated.add(resourceMarkers);
+				}
+			}
+		}
+
+		this.cachedSortedResources = undefined;
+		if (change.added.size || change.removed.size || change.updated.size) {
+			this._onDidChange.fire(change);
+		}
+	}
+
+	private id(...values: (string | number)[]): string {
+		const hasher = new Hasher();
+		for (const value of values) {
+			hasher.hash(value);
+		}
+		return `${hasher.value}`;
+	}
+
+	dispose(): void {
+		this._onDidChange.dispose();
+		this.resourcesByUri.clear();
+	}
+}

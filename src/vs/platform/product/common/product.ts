b4640cde64bf233f95f900b1071ad4755d9f1c58
Toronto00 <66029280+Toronto00@users.noreply.github.com>
2020-11-13 14:05:36 +0100

    Initialize chrmarti/alpine

---
+++ src/vs/platform/product/common/product.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+
+export const IProductService = createDecorator<IProductService>('productService');
+
+export interface IProductService {
+	_serviceBrand: any;
+
+	version: string;
+	commit?: string;
+
+	nameLong: string;
+	urlProtocol: string;
+	extensionAllowedProposedApi: string[];
+	uiExtensions?: string[];
+
+	enableTelemetry: boolean;
+	extensionsGallery?: {
+		serviceUrl: string;
+		itemUrl: string;
+		controlUrl: string;
+		recommendationsUrl: string;
+	};
+
+	sendASmile?: {
+		reportIssueUrl: string;
+		requestFeatureUrl: string;
+	};
+}

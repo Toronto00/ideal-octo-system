18b006e260a342bf70cd2eb5e80b83c2901db29d
Toronto00 <66029280+Toronto00@users.noreply.github.com>
2020-11-13 15:46:31 +0100

    Initialize rebornix/groupUndoRedo

---
+++ src/vs/workbench/contrib/extensions/browser/keymapRecommendations.ts
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExtensionRecommendations, ExtensionRecommendation, PromptedExtensionRecommendations } from 'vs/workbench/contrib/extensions/browser/extensionRecommendations';
+import { IProductService } from 'vs/platform/product/common/productService';
+import { ExtensionRecommendationReason } from 'vs/workbench/services/extensionRecommendations/common/extensionRecommendations';
+
+export class KeymapRecommendations extends ExtensionRecommendations {
+
+	private _recommendations: ExtensionRecommendation[] = [];
+	get recommendations(): ReadonlyArray<ExtensionRecommendation> { return this._recommendations; }
+
+	constructor(
+		promptedExtensionRecommendations: PromptedExtensionRecommendations,
+		@IProductService private readonly productService: IProductService,
+	) {
+		super(promptedExtensionRecommendations);
+	}
+
+	protected async doActivate(): Promise<void> {
+		if (this.productService.keymapExtensionTips) {
+			this._recommendations = this.productService.keymapExtensionTips.map(extensionId => (<ExtensionRecommendation>{
+				extensionId: extensionId.toLowerCase(),
+				reason: {
+					reasonId: ExtensionRecommendationReason.Application,
+					reasonText: ''
+				}
+			}));
+		}
+	}
+
+}
+
